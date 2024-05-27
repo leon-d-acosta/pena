4b1cb1d2e0b39b9d6591c7ad509c411c924ab090
leon-d-acosta <leon.dav.acosta@gmail.com>
2024-05-27 14:43:21 +0100

    comentarios en todos los archivos que faltaban

--- pena_aventura/lib/views/perfil/perfil.dart
+++ pena_aventura/lib/views/perfil/perfil.dart
@@ -1,64 +1,63 @@
-import 'dart:convert';
+import 'dart:convert'; // Importa a biblioteca para manipulação de dados JSON
 
-import 'package:PenaAventura/views/cores/cor.dart';
-import 'package:PenaAventura/views/login/login.dart';
-import 'package:PenaAventura/views/perfil/updates_perfil/updateApelido.dart';
-import 'package:PenaAventura/views/perfil/updates_perfil/updateNome.dart';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
+import 'package:PenaAventura/views/cores/cor.dart'; // Importa o módulo de cores personalizado
+import 'package:PenaAventura/views/login/login.dart'; // Importa a tela de login
+import 'package:PenaAventura/views/perfil/updates_perfil/updateApelido.dart'; // Importa a tela para atualizar o apelido
+import 'package:PenaAventura/views/perfil/updates_perfil/updateNome.dart'; // Importa a tela para atualizar o nome
+import 'package:flutter/material.dart'; // Importa a biblioteca principal do Flutter para construção de UI
+import 'package:shared_preferences/shared_preferences.dart'; // Importa a biblioteca para manipulação de preferências compartilhadas
+import 'package:http/http.dart' as http; // Importa a biblioteca para fazer requisições HTTP
 
 class Perfil extends StatefulWidget {
-  const Perfil({super.key});
+  const Perfil({super.key}); // Construtor da classe Perfil
 
   @override
-  State<Perfil> createState() => _PerfilState();
+  State<Perfil> createState() => _PerfilState(); // Cria o estado da tela Perfil
 }
 
 class _PerfilState extends State<Perfil> {
-  late Future<List<dynamic>> _perfilData;
+  late Future<List<dynamic>> _perfilData; // Declaração de uma variável Future para armazenar os dados do perfil
 
   @override
   void initState() {
-    _refreshView(); // Llamada para actualizar el estado al iniciar
-    super.initState();
+    _refreshView(); // Chama a função para atualizar a visão ao iniciar
+    super.initState(); // Chama o initState da classe pai
   }
 
   void _refreshView() {
     setState(() {
-      _perfilData = _getData(); // Llama a _getData() para obtener los datos actualizados
+      _perfilData = _getData(); // Chama _getData() para obter os dados atualizados
     });
   }
 
- void ScaffoldChanger(String field, snap) {
-  WidgetBuilder bottom = (BuildContext context) {
-    switch (field) {
-      case 'nome':
-        return ChangeNomeAlert(nome: snap);
-      case 'apelido':
-        return ChangeApelido(apelido: snap);
-      default:
-        throw Exception('Invalid field: $field');
-    }
-  };
+  void ScaffoldChanger(String field, snap) {
+    // Função para navegar para a tela de alteração de nome ou apelido
+    WidgetBuilder bottom = (BuildContext context) {
+      switch (field) {
+        case 'nome':
+          return ChangeNomeAlert(nome: snap); // Retorna a tela para alterar o nome
+        case 'apelido':
+          return ChangeApelido(apelido: snap); // Retorna a tela para alterar o apelido
+        default:
+          throw Exception('Invalid field: $field'); // Lança uma exceção para campos inválidos
+      }
+    };
 
-  Navigator.push(context, MaterialPageRoute(builder: bottom))
-    .then((value) {
-      _refreshView(); // Llama a _refreshView() después de que ChangeLocalidadeAlert se cierre
+    Navigator.push(context, MaterialPageRoute(builder: bottom)).then((value) {
+      _refreshView(); // Chama _refreshView() após fechar a tela de alteração
     });
-}
-
+  }
 
   void _logout() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.remove('id');
-    await prefs.remove('email');
-    await prefs.remove('palavra-passe');
+    SharedPreferences prefs = await SharedPreferences.getInstance(); // Obtém uma instância de SharedPreferences
+    await prefs.remove('id'); // Remove o ID do usuário
+    await prefs.remove('email'); // Remove o email do usuário
+    await prefs.remove('palavra-passe'); // Remove a palavra-passe do usuário
 
     Navigator.pushAndRemoveUntil(
       context,
-      MaterialPageRoute(builder: ((context) => Login())),
-      (route) => false,
+      MaterialPageRoute(builder: ((context) => Login())), // Navega para a tela de login
+      (route) => false, // Remove todas as rotas anteriores
     );
   }
 
@@ -67,72 +66,80 @@ class _PerfilState extends State<Perfil> {
     return Stack(
       children: [
         Container(
-          color: c.branco,
-          height: double.infinity,
-          padding: const EdgeInsets.all(20),
+          color: c.branco, // Define a cor de fundo do container
+          height: double.infinity, // Define a altura como infinita
+          padding: const EdgeInsets.all(20), // Define o padding
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center, // Centraliza o conteúdo verticalmente
             children: [
               Container(
-                child:  Center(
+                child: Center(
                   child: Column(
                     children: [
-                      Icon(Icons.account_circle, color: c.azul_1, size: MediaQuery.of(context).orientation == Orientation.portrait
-                                                                                                                    ? 100
-                                                                                                                    : 50),
-                      Text("M E U     P E R F I L", style: TextStyle(color: c.azul_1, fontSize: MediaQuery.of(context).orientation == Orientation.portrait
-                                                                                                                                            ? 30
-                                                                                                                                            : 25)),
+                      Icon(
+                        Icons.account_circle,
+                        color: c.azul_1,
+                        size: MediaQuery.of(context).orientation == Orientation.portrait ? 100 : 50,
+                      ), // Ícone do perfil
+                      Text(
+                        "M E U     P E R F I L",
+                        style: TextStyle(
+                          color: c.azul_1,
+                          fontSize: MediaQuery.of(context).orientation == Orientation.portrait ? 30 : 25,
+                        ),
+                      ), // Texto do título
                     ],
                   ),
                 ),
               ),
               Container(
-                margin: const EdgeInsets.only(top: 10),
+                margin: const EdgeInsets.only(top: 10), // Define a margem superior
                 height: MediaQuery.of(context).orientation == Orientation.portrait
-                                                                            ?MediaQuery.of(context).size.height / 4.75
-                                                                            :MediaQuery.of(context).size.height / 2.15,
+                    ? MediaQuery.of(context).size.height / 4.75
+                    : MediaQuery.of(context).size.height / 2.15, // Define a altura conforme a orientação
                 child: FutureBuilder(
-                  future: _perfilData,
+                  future: _perfilData, // Usa a variável Future _perfilData
                   builder: (BuildContext context, AsyncSnapshot snapshot) {
                     if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Center(
-                        child: CircularProgressIndicator(),
+                        child: CircularProgressIndicator(), // Mostra um indicador de progresso enquanto carrega
                       );
                     }
                     if (snapshot.hasError) {
                       return const Center(
-                        child: Text("Error fetching data"),
+                        child: Text("Error fetching data"), // Mostra uma mensagem de erro se houver falha
                       );
                     }
-                    List snap = snapshot.data;
-                    List<String> fields = ['nome', 'apelido', 'email'];
+                    List snap = snapshot.data; // Obtém os dados do snapshot
+                    List<String> fields = ['nome', 'apelido', 'email']; // Campos do perfil
                     return Container(
                       decoration: BoxDecoration(
                         color: c.azul_1,
                         borderRadius: BorderRadius.circular(10),
-                      ),
+                      ), // Decoração do container
                       height: MediaQuery.of(context).size.height,
                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        crossAxisAlignment: CrossAxisAlignment.stretch, // Estica os itens para ocupar o espaço disponível
                         children: List.generate(fields.length, (index) {
-                          String field = fields[index];
+                          String field = fields[index]; // Campo atual
                           return GestureDetector(
                             onTap: () {
                               if (field != 'email') {
-                                ScaffoldChanger(field, snap[0][field]);
+                                ScaffoldChanger(field, snap[0][field]); // Chama ScaffoldChanger para campos editáveis
                               }
                             },
                             child: ListTile(
                               title: Text(
                                 snap[0][field],
-                                style: const TextStyle(color: c.branco),
+                                style: const TextStyle(color: c.branco), // Estilo do texto
                               ),
-                              trailing: field != 'email' ? const Icon(Icons.arrow_forward_ios, color: c.branco,) : const Icon(Icons.alternate_email, color: c.branco,),
+                              trailing: field != 'email'
+                                  ? const Icon(Icons.arrow_forward_ios, color: c.branco,)
+                                  : const Icon(Icons.alternate_email, color: c.branco,), // Ícone conforme o campo
                             ),
                           );
                         }),
-),
+                      ),
                     );
                   },
                 ),
@@ -141,26 +148,26 @@ class _PerfilState extends State<Perfil> {
           ),
         ),
         Positioned(
-          top: 40,
-          right: 20,
+          top: 40, // Define a posição superior
+          right: 20, // Define a posição à direita
           child: GestureDetector(
-            onTap: _logout,
+            onTap: _logout, // Chama a função de logout ao tocar
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.redAccent,
-                borderRadius: BorderRadius.circular(50),
+                color: Colors.redAccent, // Define a cor de fundo
+                borderRadius: BorderRadius.circular(50), // Define os cantos arredondados
               ),
-              padding: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10), // Define o padding
               child: const Row(
                 children: [
                   Text(
                     'LOGOUT  ',
-                    style: TextStyle(color: c.branco, fontSize: 15, fontWeight: FontWeight.bold),
+                    style: TextStyle(color: c.branco, fontSize: 15, fontWeight: FontWeight.bold), // Estilo do texto
                   ),
                   Icon(
                     Icons.logout,
                     size: 20,
-                    color: c.branco,
+                    color: c.branco, // Cor do ícone
                   ),
                 ],
               ),
@@ -172,16 +179,16 @@ class _PerfilState extends State<Perfil> {
   }
 
   Future<List<dynamic>> _getData() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    int id = prefs.getInt('id') ?? 0;
+    SharedPreferences prefs = await SharedPreferences.getInstance(); // Obtém uma instância de SharedPreferences
+    int id = prefs.getInt('id') ?? 0; // Obtém o ID do usuário
 
-    var url = Uri.parse('https://lavandaria.oxb.pt/index.php/get_perfil_app');
-    var response = await http.post(url, body: {'id_utilizador': id.toString()});
+    var url = Uri.parse('https://lavandaria.oxb.pt/index.php/get_perfil_app'); // URL da API para obter os dados do perfil
+    var response = await http.post(url, body: {'id_utilizador': id.toString()}); // Faz uma requisição POST com o ID do usuário
 
     if (response.statusCode == 200) {
-      return json.decode(response.body);
+      return json.decode(response.body); // Decodifica e retorna os dados se a requisição for bem-sucedida
     } else {
-      throw Exception('Error al obtener los datos');
+      throw Exception('Error al obtener los datos'); // Lança uma exceção se houver erro na requisição
     }
   }
 }
--- pena_aventura/lib/views/postos/postos.dart
+++ pena_aventura/lib/views/postos/postos.dart
@@ -1,85 +1,84 @@
-import 'dart:convert';
+import 'dart:convert'; // Importa a biblioteca 'dart:convert' para manipulação de dados JSON.
 
-import 'package:PenaAventura/views/cores/cor.dart';
-import 'package:PenaAventura/views/postos/qr_tarefa/qr_scanner.dart';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
+import 'package:PenaAventura/views/cores/cor.dart'; // Importa um arquivo local com cores personalizadas.
+import 'package:PenaAventura/views/postos/qr_tarefa/qr_scanner.dart'; // Importa o widget do scanner QR.
+import 'package:flutter/material.dart'; // Importa o pacote Flutter para construir a interface de usuário.
+import 'package:shared_preferences/shared_preferences.dart'; // Importa o pacote para gerenciamento de preferências compartilhadas.
+import 'package:http/http.dart' as http; // Importa o pacote HTTP para realizar solicitações web.
 
-class Postos extends StatefulWidget {
+class Postos extends StatefulWidget { // Define uma classe de widget com estado chamada 'Postos'.
   const Postos({super.key});
 
   @override
-  State<Postos> createState() => _PostosState();
+  State<Postos> createState() => _PostosState(); // Cria um estado para o widget 'Postos'.
 }
 
 class _PostosState extends State<Postos> {
-  late Future<List<dynamic>> _futureData;
+  late Future<List<dynamic>> _futureData; // Declara uma variável para armazenar os dados futuros.
 
   @override
   void initState() {
     super.initState();
-    _futureData = _getData();
+    _futureData = _getData(); // Inicializa a variável com os dados obtidos da função '_getData'.
   }
 
-  Future<int?> _getid() async {
+  Future<int?> _getid() async { // Função para obter o ID do usuário das preferências compartilhadas.
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getInt('id');
+    return prefs.getInt('id'); // Retorna o ID armazenado nas preferências.
   }
 
-Future<List<dynamic>> _getData() async {
-    int id = (await _getid()) ?? 0;
+  Future<List<dynamic>> _getData() async { // Função para obter os dados dos postos.
+    int id = (await _getid()) ?? 0; // Obtém o ID do usuário, ou 0 se não estiver disponível.
 
-    var url = Uri.parse('https://adminpena.oxb.pt/index.php/getpostosapp');
-    var response  = await http.post(url, body: {'id': id.toString()});
+    var url = Uri.parse('https://adminpena.oxb.pt/index.php/getpostosapp'); // Define a URL da solicitação.
+    var response = await http.post(url, body: {'id': id.toString()}); // Realiza a solicitação POST com o ID do usuário.
 
-    if (response.statusCode == 200) {
-      return json.decode(response.body);
+    if (response.statusCode == 200) { // Verifica se a solicitação foi bem-sucedida.
+      return json.decode(response.body); // Decodifica os dados JSON da resposta.
     } else {
-      throw Exception('Error al obtener los datos');
+      throw Exception('Erro ao obter os dados'); // Lança uma exceção se houver um erro na solicitação.
     }
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { // Define o método de construção do widget.
     return Scaffold(
-      backgroundColor: c.branco,
+      backgroundColor: c.branco, // Define a cor de fundo da tela.
       body: Padding(
-        padding: const EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10), // Define o preenchimento ao redor do corpo.
         child: FutureBuilder(
-          future: _futureData,
-          builder: (BuildContext context, AsyncSnapshot snapshot){
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
+          future: _futureData, // Define o futuro a ser observado.
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) { // Verifica se os dados estão sendo carregados.
+              return const Center(child: CircularProgressIndicator()); // Mostra um indicador de progresso enquanto os dados são carregados.
             }
-            if (snapshot.hasError) {
-              return Center(child: Text("${snapshot.error}"),);
+            if (snapshot.hasError) { // Verifica se houve um erro ao carregar os dados.
+              return Center(child: Text("${snapshot.error}")); // Mostra a mensagem de erro.
             }
-            List<dynamic> snap = snapshot.data!;
+            List<dynamic> snap = snapshot.data; // Armazena os dados carregados.
             return GridView.builder(
-              itemCount: snap.length,
+              itemCount: snap.length, // Define o número de itens na grade.
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 4,
-                mainAxisSpacing: 15,
-                crossAxisSpacing: 15
+                crossAxisCount: MediaQuery.of(context).orientation == Orientation.portrait ? 2 : 4, // Define o número de colunas baseado na orientação da tela.
+                mainAxisSpacing: 15, // Define o espaçamento principal entre os itens.
+                crossAxisSpacing: 15 // Define o espaçamento cruzado entre os itens.
               ), 
               itemBuilder: (context, index) {
                 return GestureDetector(
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Qr_Scanner(id_posto: snap[index]['id'],))),
+                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => Qr_Scanner(id_posto: snap[index]['id']))), // Navega para a página de escaneamento QR ao tocar no item.
                   child: Container(
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5),
-                      color: c.cinza_2,
-    
+                      borderRadius: BorderRadius.circular(5), // Define os cantos arredondados do contêiner.
+                      color: c.cinza_2, // Define a cor de fundo do contêiner.
                     ),
                     width: MediaQuery.of(context).orientation == Orientation.portrait
                         ? MediaQuery.of(context).size.width * 0.5 - 15 
-                        : MediaQuery.of(context).size.width * 0.25 - 15,
+                        : MediaQuery.of(context).size.width * 0.25 - 15, // Define a largura do contêiner baseado na orientação da tela.
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center, // Alinha os itens ao centro horizontalmente.
                       children: [
-                        Container(height: MediaQuery.of(context).size.height/6, child: ClipRRect(borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)), child: Image.network(snap[index]['foto'], fit: BoxFit.cover,))),
-                        Container(margin: const EdgeInsets.only(left: 20, right: 20), child: Center(child: Text(snap[index]['nome'], style: TextStyle(color: c.branco, fontSize: 15 ),))),
+                        Container(height: MediaQuery.of(context).size.height/6, child: ClipRRect(borderRadius: BorderRadius.only(topLeft: Radius.circular(5), topRight: Radius.circular(5)), child: Image.network(snap[index]['foto'], fit: BoxFit.cover))), // Exibe a imagem do posto com cantos arredondados.
+                        Container(margin: const EdgeInsets.only(left: 20, right: 20), child: Center(child: Text(snap[index]['nome'], style: TextStyle(color: c.branco, fontSize: 15)))), // Exibe o nome do posto.
                       ],
                     ),
                   ),
@@ -91,5 +90,4 @@ Future<List<dynamic>> _getData() async {
       ),
     );
   }
-  
 }
