1f2d04afa82bddad451e1429d9858bda20688e77
leon-d-acosta <leon.dav.acosta@gmail.com>
2024-05-28 14:39:40 +0100

    agregue calendario y container donde van a ir los eventois del dia seleccionado

--- pena_aventura/lib/views/calendario/calendario.dart
+++ pena_aventura/lib/views/calendario/calendario.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:PenaAventura/views/cores/cor.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendario extends StatefulWidget {
   const Calendario({super.key});
@@ -9,10 +10,62 @@ class Calendario extends StatefulWidget {
 }
 
 class _CalendarioState extends State<Calendario> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+  ValueNotifier<List<bool>> _formatChanger = ValueNotifier<List<bool>>([]); // Lista para controlar a expansão dos painéis
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: c.branco,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TableCalendar(
+
+                firstDay: DateTime.utc(2022, 1, 1),
+                lastDay: DateTime.utc(2030, 1, 1),
+                focusedDay: DateTime.now(),
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  });
+                },
+                calendarFormat: _calendarFormat,
+                onFormatChanged: (format) {
+                  setState(() {
+                    _calendarFormat = format;
+                  });
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: _calendarFormat == CalendarFormat.twoWeeks
+                                              ? MediaQuery.of(context).size.height/1.75
+                                              : _calendarFormat == CalendarFormat.month
+                                              ? MediaQuery.of(context).size.height/2.75
+                                              : _calendarFormat == CalendarFormat.week
+                                              ? MediaQuery.of(context).size.height/1.5
+                                              :null,
+                  decoration: BoxDecoration(
+                    color: c.azul_2.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(30)
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ), 
     );
   }
 }
